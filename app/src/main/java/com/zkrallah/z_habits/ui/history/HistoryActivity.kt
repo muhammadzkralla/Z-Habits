66e4f9cd841287879a811dec286dbbd57c5ad5f3
Zkrallah <muhammad.heshamyt@gmail.com>
2023-07-04 09:57:33 +0300

    Day Details Implemented.

--- app/src/main/java/com/zkrallah/z_habits/ui/history/HistoryActivity.kt
+++ app/src/main/java/com/zkrallah/z_habits/ui/history/HistoryActivity.kt
@@ -1,17 +1,51 @@
 package com.zkrallah.z_habits.ui.history
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.app.DatePickerDialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.zkrallah.z_habits.R
 import com.zkrallah.z_habits.adapter.HistoryAdapter
 import com.zkrallah.z_habits.databinding.ActivityHistoryBinding
 import com.zkrallah.z_habits.local.entities.History
+import java.text.SimpleDateFormat
+import java.util.*
 
 class HistoryActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHistoryBinding
     private lateinit var viewModel: HistoryViewModel
+    private lateinit var dialog: AlertDialog
+    private val calendar: Calendar = Calendar.getInstance()
+    private val map = mapOf(
+        1 to "Very Bad",
+        2 to "Bad",
+        3 to "Neutral",
+        4 to "Good",
+        5 to "Very Good"
+    )
+
+    private val imagesMap = mapOf(
+        1 to R.drawable.ic_very_bad,
+        2 to R.drawable.ic_bad,
+        3 to R.drawable.ic_neutral,
+        4 to R.drawable.ic_good,
+        5 to R.drawable.ic_very_good
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,7 +69,7 @@ class HistoryActivity : AppCompatActivity() {
                 layoutManager.reverseLayout = true
                 binding.recyclerHistory.layoutManager = layoutManager
 
-                adapter.setItemClickListener(object : HistoryAdapter.OnItemClickListener{
+                adapter.setItemClickListener(object : HistoryAdapter.OnItemClickListener {
                     override fun onDeleteClicked(history: History, position: Int) {
                         viewModel.deleteHistory(history.historyId)
                         adapter.removeItem(position)
@@ -44,5 +78,124 @@ class HistoryActivity : AppCompatActivity() {
                 })
             }
         }
+    }
+
+    private fun buildDetailsDialog(date: String) {
+        viewModel.getTodayDetails(date)
+        viewModel.state.observe(this@HistoryActivity, object : Observer<Boolean> {
+            @SuppressLint("SetTextI18n")
+            override fun onChanged(value: Boolean) {
+                if (value) {
+                    val historyResponse = viewModel.dayHistory.value
+                    val moodResponse = viewModel.moodStatus.value
+
+                    var countPerDay = 0.0
+                    var countDone = 0.0
+
+                    if (historyResponse != null) {
+                        for (item in historyResponse) {
+                            countPerDay += item.countPerDay
+                            countDone += item.countDone
+                        }
+                    }
+
+                    val inflater = layoutInflater
+                    val dialogView = inflater.inflate(R.layout.day_details_dialog, null)
+                    val builder = AlertDialog.Builder(this@HistoryActivity, R.style.MyDialogTheme)
+                    builder.setNegativeButton("OK"){_, _ ->
+                        dialog.dismiss()
+                    }
+
+                    builder.setCancelable(true)
+
+                    val recycler =
+                        dialogView.findViewById<RecyclerView>(R.id.recycler_habit_history)
+                    val progress = dialogView.findViewById<TextView>(R.id.progress)
+                    val moodCard = dialogView.findViewById<CardView>(R.id.mood_card)
+                    val moodDate = dialogView.findViewById<TextView>(R.id.date)
+                    val moodValue = dialogView.findViewById<TextView>(R.id.value)
+                    val moodMessage = dialogView.findViewById<TextView>(R.id.message)
+                    val moodImage = dialogView.findViewById<ImageView>(R.id.mood_image)
+
+                    val layoutManager =
+                        LinearLayoutManager(
+                            this@HistoryActivity,
+                            LinearLayoutManager.VERTICAL,
+                            true
+                        )
+                    layoutManager.stackFromEnd = true
+                    layoutManager.reverseLayout = true
+
+                    val adapter = HistoryAdapter(historyResponse as MutableList<History>)
+                    adapter.setItemClickListener(object : HistoryAdapter.OnItemClickListener {
+                        override fun onDeleteClicked(history: History, position: Int) {
+                            viewModel.deleteHistory(history.historyId)
+                            adapter.removeItem(position)
+                        }
+
+                    })
+
+                    recycler.adapter = adapter
+                    recycler.layoutManager = layoutManager
+                    if (moodResponse != null) {
+                        moodDate.text = moodResponse.date
+                        moodValue.text = map[moodResponse.value]
+                        moodMessage.text = moodResponse.message
+                        Glide.with(this@HistoryActivity).load(imagesMap[moodResponse.value])
+                            .into(moodImage)
+                    } else moodCard.visibility = View.GONE
+
+                    if (countPerDay != 0.0) {
+                        val percentage = (countDone / countPerDay) * 100
+                        val number2digits: Double =
+                            String.format("%.2f", percentage).toDouble()
+                        progress.text = "Progress : $number2digits%"
+                    }
+
+                    viewModel.clearDetails()
+                    viewModel.state.removeObserver(this)
+
+                    builder.setView(dialogView)
+                    dialog = builder.create()
+                    dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+                    dialog.show()
+
+                }
+            }
+
+        })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        if (id == R.id.search) {
+            val year = calendar.get(Calendar.YEAR)
+            val month = calendar.get(Calendar.MONTH)
+            val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+            val datePickerDialog = DatePickerDialog(
+                this,
+                { _, yearSelected, monthOfYear, dayOfMonth ->
+
+                    val newCalendar = Calendar.getInstance()
+                    val formatter = SimpleDateFormat("yyyy-MM-dd", Locale.ROOT)
+                    newCalendar.set(yearSelected, monthOfYear, dayOfMonth)
+                    val date = formatter.format(newCalendar.time)
+                    buildDetailsDialog(date)
+
+                },
+
+                year,
+                month,
+                day
+            )
+            datePickerDialog.show()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
--- app/src/main/java/com/zkrallah/z_habits/ui/history/HistoryViewModel.kt
+++ app/src/main/java/com/zkrallah/z_habits/ui/history/HistoryViewModel.kt
@@ -6,7 +6,9 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.zkrallah.z_habits.local.HabitsDatabase
 import com.zkrallah.z_habits.local.entities.History
+import com.zkrallah.z_habits.local.entities.Mood
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
 import kotlinx.coroutines.launch
 
 class HistoryViewModel : ViewModel() {
@@ -14,6 +16,12 @@ class HistoryViewModel : ViewModel() {
 
     private val _history = MutableLiveData<List<History>>()
     val history: LiveData<List<History>> = _history
+    private val _dayHistory = MutableLiveData<List<History>?>()
+    val dayHistory: LiveData<List<History>?> = _dayHistory
+    private val _moodStatus = MutableLiveData<Mood?>()
+    val moodStatus: LiveData<Mood?> = _moodStatus
+    private val _state = MutableLiveData(false)
+    val state = _state
 
     fun getHistory() {
         viewModelScope.launch(Dispatchers.IO) {
@@ -25,5 +33,20 @@ class HistoryViewModel : ViewModel() {
         viewModelScope.launch (Dispatchers.IO){
             database.historyDAO().deleteHistory(historyId)
         }
+    }
+
+    fun getTodayDetails(date: String){
+        viewModelScope.launch (Dispatchers.IO){
+            val historyJob = async{ database.historyDAO().getAllTodayHistory(date) }
+            val moodJob = async { database.moodDAO().getTodayMood(date) }
+            _dayHistory.postValue(historyJob.await())
+            _moodStatus.postValue(moodJob.await())
+            _state.postValue(true)
+        }
+    }
+
+    fun clearDetails(){
+        _dayHistory.value = null
+        _state.value = false
     }
 }
